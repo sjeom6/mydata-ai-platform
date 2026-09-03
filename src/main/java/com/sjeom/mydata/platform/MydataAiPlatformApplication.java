191ffcc4b8aac5803566f79d4b0b0250776f8de9
sjeom6 <sjeom6@gmail.com>
2026-09-03 09:23:11 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/sjeom/mydata/platform/MydataAiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sjeom.mydata.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydataAiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydataAiPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjeom/mydata/platform/MydataAiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjeom.mydata.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydataAiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
